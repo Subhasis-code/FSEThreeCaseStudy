0eccd7d641f2fa60d997d48b7a270433cbd4b171
cogdotnet1564 <cogdotnet1564@CTSDOTNET998>
2023-04-10 09:17:35 +0530

    file name update

---
+++ src/Services/Member/Member.Application/Features/Members/Queries/GetMemberTaskList/GetMemberTaskListQueryHaldler.cs
@@ -0,0 +1,30 @@
+﻿using AutoMapper;
+using MediatR;
+using Member.Application.Contracts.Persistance;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Member.Application.Features.Members.Queries.GetMemberTaskList
+{
+    public class GetMemberTaskListQueryHaldler : IRequestHandler<GetMemberTaskListQuery, List<MemberTaskVm>>
+    {
+        private readonly IMemberTaskRepository _memberTaskRepository;
+        private readonly IMapper _mapper;
+
+        public GetMemberTaskListQueryHaldler(IMemberTaskRepository memberTaskRepository, IMapper mapper)
+        {
+            _memberTaskRepository = memberTaskRepository ?? throw new ArgumentNullException(nameof(memberTaskRepository));
+            _mapper = mapper ?? throw new ArgumentNullException(nameof(mapper));
+        }
+
+        public async Task<List<MemberTaskVm>> Handle(GetMemberTaskListQuery request, CancellationToken cancellationToken)
+        {
+            var response = await _memberTaskRepository.GetTaskListByMemberID(request.MemberId);
+            return _mapper.Map<List<MemberTaskVm>>(response);
+        }
+    }
+}
