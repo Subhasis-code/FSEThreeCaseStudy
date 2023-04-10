35594496ac15d7251a173aa9c448f729c36b4e5d
cogdotnet1564 <cogdotnet1564@CTSDOTNET998>
2023-04-10 08:58:40 +0530

    member task vm class update

--- src/Services/Member/Member.Application/Features/Members/Queries/GetMemberTaskList/MemberTaskVm.cs
+++ src/Services/Member/Member.Application/Features/Members/Queries/GetMemberTaskList/MemberTaskVm.cs
@@ -8,5 +8,11 @@ namespace Member.Application.Features.Members.Queries.GetMemberTaskList
 {
     public class MemberTaskVm
     {
+        public string MemberName { get; set; }
+        public int MemberId { get; set; }
+        public string TaskName { get; set; }
+        public string Deliverables { get; set; }
+        public DateTime TaskStartDate { get; set; }
+        public DateTime TaskEndtDate { get; set; }
     }
 }
