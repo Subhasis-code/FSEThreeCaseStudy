a2281949eb12966ebda1654db09e0078c3fba998
cogdotnet1564 <cogdotnet1564@CTSDOTNET998>
2023-04-22 10:59:38 +0530

    infrustructure layer changes

--- src/Services/Member/Member.Infrastructure/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace Member.Infrastructure
-{
-    public class Class1
-    {
-    }
-}
---
+++ src/Services/Member/Member.Infrastructure/Persistence/MemberContext.cs
@@ -0,0 +1,39 @@
+﻿using Member.Domain.Common;
+using Member.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Member.Infrastructure.Persistence
+{
+    public class MemberContext : DbContext
+    {
+        public MemberContext(DbContextOptions<MemberContext> options) : base(options)
+        {
+        }
+        public DbSet<TeamMember> teamMembers;
+        public override Task<int> SaveChangesAsync(bool acceptAllChangesOnSuccess, CancellationToken cancellationToken = default)
+        {
+            foreach(var entry in ChangeTracker.Entries<EntityBase>())
+            {
+                switch(entry.State)
+                {
+                    case EntityState.Added:
+                        entry.Entity.CreatedBy = "cmanager";
+                        entry.Entity.CreatedDate = DateTime.Now;
+                        break;
+                    case EntityState.Modified:
+                        entry.Entity.LastModifiedBy = "umanager";
+                        entry.Entity.LastModifiedDate = DateTime.Now;
+                        break;
+
+                }
+            }
+            return base.SaveChangesAsync(acceptAllChangesOnSuccess, cancellationToken);
+        }
+    }
+}
